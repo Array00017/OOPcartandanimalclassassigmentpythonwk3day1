b90617f86946a94b3ca2c4f17f847622f79a19bb
Arnold <Array0824@gmail.com>
2023-12-14 00:32:43 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,48 @@
+
+#Exercise 1 - Write a Python class for an Animal that has a name and energy attributes. The animal class should also have methods for eat, sleep, and play that will take in an integer and increase/decrease the energy of the animal with a formatted print statement
+
+# Example 1
+# buddy = Animal('Buddy', 10)
+# buddy.play(5) -> "Buddy is playing for 5 minutes. His energy is now 5"
+# buddy.sleep(10) -> "Buddy is sleeping for 10 minutes. His energy is now 15"
+
+class Animal:
+    energy = 100
+    name = buddy
+
+def __eat__(name,):
+    print(f"{name} is at {energy}level"):
+
+def __sleep__(name):
+    print(f"{name} is sleeping"):
+
+def __play__(name):
+    print(f"{name} is playing"):
+
+
+
+#Exercise 2 - Turn the shopping cart program into an object-oriented program
+# Create a class called cart that retains items and has methods to add, remove, and show
+
+
+class Cart:
+ 
+   list = []
+
+   print input("what would you like to purchase"):
+   print input.remove = ("remove"):
+  
+   
+   def __add__(name,item,price):
+        if item == ("add"):
+        list.append and print(f"{item} added and {price}"):
+
+    def __remove__(item):
+        if item == item and input == "remove"
+        item.remove from list:
+        print(f"item removed"):
+        
+    def __show__(item,price):
+        print(f"{item} + {list}"):
+        print(f"Total = {price}"):
+        
